76f50bc4d7b925a3cbe31201f38eeed2770fb90f
Gabriel Oliveira <gabriel@oldbyte.com.br>
2023-08-29 09:23:00 -0300

    Adicionado ID's aos botões de fechar o modal

--- Listing/Resources/views/confirmation-modal.blade.php
+++ Listing/Resources/views/confirmation-modal.blade.php
@@ -3,15 +3,15 @@
         <div class="modal-content">
             <div class="modal-header">
                 <h3 class="modal-title" id="confirmationModalLabel">Confirmação</h3>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
+                <button type="button" id="btnXModal" class="close btn btn-outline-secondary py-1 px-2" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
                 Campo não configurado. Por favor, entre em contato com o suporte.
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal" data-confirmation="cancel">Cancelar</button>
+                <button type="button" id="btnCloseModal" class="btn btn-default" data-dismiss="modal" data-confirmation="cancel">Cancelar</button>
                 <button type="button" class="btn btn-danger btnConfirm" data-confirmation="confirm">Confirmar</button>
             </div>
         </div>
